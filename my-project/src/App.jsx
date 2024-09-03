b600c11a7a022411dfb5ff890f6639a4c20c12b6
Shivendra Tripathi <shivendraking19@gmail.com>
2024-09-03 19:07:59 +0530

    first work

---
+++ my-project/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "flowbite-react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+      <Button>he</Button>
+    </h1>
+  );
+}
