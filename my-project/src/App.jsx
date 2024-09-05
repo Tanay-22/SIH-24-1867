aef01776bb7609a0ac8699c279cd94fa35b929a4
Shivendra Tripathi <shivendraking19@gmail.com>
2024-09-05 19:03:22 +0530

    router added

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,11 +1,14 @@
 import { Button } from "flowbite-react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-      <Button>he</Button>
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
