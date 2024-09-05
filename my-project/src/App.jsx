b4d269fc28a8f2fc32777756e6ecab7a23f8c3f8
deepak04iiitn <dky422003@gmail.com>
2024-09-05 20:17:50 +0530

    completed navbar

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,12 +2,20 @@ import { Button } from "flowbite-react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Navbar from "./components/Navbar";
+import Disaster from "./pages/disaster/Disaster";
+import Dashboard from "./pages/dashboard/Dashboard";
+import AboutUs from "./pages/aboutUs/AboutUs";
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/show-disaster" element={<Disaster />} />
+        <Route path="/show-dashboard" element={<Dashboard />} />
+        <Route path="/about-us" element={<AboutUs />} />
       </Routes>
     </BrowserRouter>
   );
--- my-project/src/pages/home/Home.jsx
+++ my-project/src/pages/home/Home.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Footer from "../../components/Footer";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="pt-20">
+      Home
+
+      <Footer />
+    </div>
+  )
+  
 }
