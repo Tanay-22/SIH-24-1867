db9d18b16178b7d8ffb276836def251a832bb5c1
deepak04iiitn <dky422003@gmail.com>
2024-09-07 19:56:48 +0530

    added changes

--- my-project/src/pages/home/Home.jsx
+++ my-project/src/pages/home/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="mt-8">
 
       <div className="h-[500px] w-full">
-        <Carousel pauseOnHover>
+        <Carousel pauseOnHover slideInterval={2000}>
           <img src="/disaster.jpg" alt="Disaster 1" />
           <img src="/disaster2.jpg" alt="Disaster 2" />
           <img src="/disaster3.jpg" alt="Disaster 3" />
